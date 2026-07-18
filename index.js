0502344de183a17ff530c05b467121cfd01be26b
Your Name <bilalzainab123agmail.com>
2026-07-18 13:57:15 +0500

    Stage 2: Added read endpoints

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+let tasks = [
+{
+    id:1,
+    title:"study node.js",
+    done: false,
 
-app.get('/',(req,res) => {
-    res.json({
-        name : "task API",
-        version: "1.0",
-        endpoint: ["/tasks"]
-    })
+
+},
+{
+    id:2,
+    title:"complete asisgnment",
+    done: true
+
+
+},
+{
+    id:3,
+    title:"watch backend lecture",
+    done: false,
+
+
+},
+]
+app.get('/tasks',(req,res) => {
+    res.status(200).json(tasks);
 });
-app.get('/health',(req,res) => {
-       res.json({
-        status:"ok"
-       })
+app.get('/tasks/:id',(req,res) => {
+       const  id = parseInt(req.params.id);
+    //    console.log(id)
+       const task = tasks.find((task) => task.id == id);
+    if(!task) {
+        return res.status(404).json({
+            error:`task${id} not found`
+        })
+    }
+    res.status(200).json(task);
 })
 app.listen(port,()=> {
     console.log(`server running on port ${port}`)
